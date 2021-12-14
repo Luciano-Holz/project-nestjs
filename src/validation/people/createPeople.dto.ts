62e1b56f012bc0f47ba6bd1a0095e2ccacf1eb23
Luciano Holz <luciano.holz@hotmail.com>
2021-12-14 18:17:35 -0300

    feat: create file with validations to create people

---
+++ src/validation/people/createPeople.dto.ts
@@ -0,0 +1,68 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
+import * as Joi from 'joi';
+import { CREATE, JoiSchema, JoiSchemaOptions } from 'nestjs-joi';
+
+@JoiSchemaOptions({
+  allowUnknown: false,
+})
+export class CreatePeopleDto {
+  @ApiProperty()
+  @JoiSchema(Joi.string().trim().alphanum().min(2).max(30).required())
+  @JoiSchema([CREATE], Joi.string().trim().alphanum().min(2).max(30).required())
+  @IsString()
+  @MinLength(3)
+  nome: string;
+
+  @ApiProperty()
+  @JoiSchema(Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required())
+  @JoiSchema(
+    [CREATE],
+    Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+  )
+  @IsString()
+  @MinLength(6)
+  senha: string;
+
+  @ApiProperty()
+  @JoiSchema(Joi.string().trim().length(14).required())
+  @JoiSchema([CREATE], Joi.string().trim().length(14).required())
+  @IsString()
+  cpf: string;
+
+  @ApiProperty()
+  @JoiSchema(Joi.string().trim().required())
+  @JoiSchema([CREATE], Joi.string().trim().required())
+  @IsString()
+  data_nascimento: string;
+
+  @ApiProperty()
+  @JoiSchema(
+    Joi.string()
+      .trim()
+      .email({
+        minDomainSegments: 2,
+        tlds: { allow: ['com', 'net', 'br'] },
+      })
+      .required(),
+  )
+  @JoiSchema(
+    [CREATE],
+    Joi.string()
+      .trim()
+      .email({
+        minDomainSegments: 2,
+        tlds: { allow: ['com', 'net', 'br'] },
+      })
+      .required(),
+  )
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @JoiSchema(Joi.string().trim().valid('sim', 'nao').required())
+  @JoiSchema([CREATE], Joi.string().trim().valid('sim', 'nao').required())
+  @IsString()
+  habilitado: string;
+}
