d98898656ea9d2b21966c88cc3dde1d3d7e534ff
Luciano Holz <luciano.holz@hotmail.com>
2021-12-14 19:30:20 -0300

    chore: external regex and add messages

--- src/validation/people/createPeople.dto.ts
+++ src/validation/people/createPeople.dto.ts
@@ -2,31 +2,22 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 import * as Joi from 'joi';
 import { CREATE, JoiSchema, JoiSchemaOptions } from 'nestjs-joi';
+import { cpfRegex, senhaRegex } from 'src/utils/regex';
 
 @JoiSchemaOptions({
   allowUnknown: false,
 })
 export class CreatePeopleDto {
   @ApiProperty()
-  @JoiSchema(Joi.string().trim().alphanum().min(2).max(30).required())
-  @JoiSchema([CREATE], Joi.string().trim().alphanum().min(2).max(30).required())
+  @JoiSchema(Joi.string().trim().min(2).max(30).required())
+  @JoiSchema([CREATE], Joi.string().trim().min(2).max(30).required())
   @IsString()
   @MinLength(3)
   nome: string;
 
   @ApiProperty()
-  @JoiSchema(Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required())
-  @JoiSchema(
-    [CREATE],
-    Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-  )
-  @IsString()
-  @MinLength(6)
-  senha: string;
-
-  @ApiProperty()
-  @JoiSchema(Joi.string().trim().length(14).required())
-  @JoiSchema([CREATE], Joi.string().trim().length(14).required())
+  @JoiSchema(Joi.string().trim().regex(cpfRegex).message('"cpf" has an Invalid format, it should be xxx.xxx.xxx-xx').required())
+  @JoiSchema([CREATE], Joi.string().trim().regex(cpfRegex).message('"cpf" has an Invalid format, it should be xxx.xxx.xxx-xx').required())
   @IsString()
   cpf: string;
 
@@ -59,6 +50,16 @@ export class CreatePeopleDto {
   @IsString()
   @IsEmail()
   email: string;
+
+  @ApiProperty()
+  @JoiSchema(Joi.string().regex(senhaRegex).message('"senha" should have at least three alphanumeric characters').required())
+  @JoiSchema(
+    [CREATE],
+    Joi.string().regex(senhaRegex).message('"senha" should have at least three alphanumeric characters').required(),
+  )
+  @IsString()
+  @MinLength(6)
+  senha: string;
 
   @ApiProperty()
   @JoiSchema(Joi.string().trim().valid('sim', 'nao').required())
